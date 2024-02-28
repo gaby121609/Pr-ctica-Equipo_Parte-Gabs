d9d9abe29a4db17008c94c00de68bec77ec7d38d
Z4AC22 <160799042+Z4AC22@users.noreply.github.com>
2024-02-28 17:43:06 -0600

    test28.py

---
+++ test28.py
@@ -0,0 +1,4 @@
+#instrucciones:
+#1. Te pida tus datos como nombre apelidos y edad
+#2. cree una curp con los datos ... en realidad son solo la inicial de tu nombre, tu edad y la inicial de tu apellido
+#3. agregar cualquier funcion

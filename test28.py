09ca25b06f7c6f94543ed2685af107477624d1e5
unknown <anunezb004@alumno.uaemex.mx>
2024-02-28 18:32:34 -0600

    Codigo de gaby

--- test28.py
+++ test28.py
@@ -2,3 +2,9 @@
 #1. Te pida tus datos como nombre apelidos y edad
 #2. cree una curp con los datos ... en realidad son solo la inicial de tu nombre, tu edad y la inicial de tu apellido
 #3. agregar cualquier funcion
+x= str(input("Ingresa tu nombre "))
+g= str(input("Ingresa tu apellido "))
+z= int(input("Ingresa tu edad "))
+b= x[0:1]
+c= g[0:1]
+p= print("Esta es tu curp 😀 : ",b,z,c)
